8dceb8f77cd86d848c5b0e78c315d9f3605fbaf2
RasmusLC1 <145757764+RasmusLC1@users.noreply.github.com>
2024-06-24 23:32:55 +0200

    Comments and readding active item to items

--- scripts/Chest/inventory.py
+++ scripts/Chest/inventory.py
@@ -17,12 +17,13 @@ class Inventory:
 
     def Update(self, offset = (0,0)):
         Inventory.Active_Item(self, offset)
-        
+        # Check if an item is being dragged
         for j in range(self.y_size):
             for i in range(self.x_size):
                 if self.inventory[j][i]:
                     self.inventory[j][i].Update_Animation()
                     if self.game.mouse.left_click:
+                        # Check for collision with inventory slot
                         if self.inventory[j][i].rect().colliderect(self.game.mouse.mouse_rect()):
                             self.item_clicked += 1
                             if self.game.mouse.hold_down_left > 10:
@@ -33,23 +34,31 @@ class Inventory:
                                 self.inventory[j][i] = None
                             
 
-                    
+    # Active item is an item being dragged
     def Active_Item(self, offset = (0,0)):
+        # Check if there is an active item
         if self.active_item:
-            item_out_of_bounds = self.active_item.Move_Legal(self.game.mouse, self.game.player.pos, self.game.tilemap)
+            # Check for out of bounds
+            item_out_of_bounds = self.active_item.Move_Legal(self.game.mouse.mpos, self.game.player.pos, self.game.tilemap)
             if item_out_of_bounds == False:
+                # Render with fadeout depending on distance
                 self.active_item.render_out_of_bounds(self.game.player.pos, self.game.mouse.mpos, self.game.display, offset)  
+                # Return the item to Inventory
                 if self.game.mouse.left_click == False:
                     x = self.saved_position[1] * self.size[1] + self.game.screen_width / 2 / self.game.render_scale - 40
                     y = self.saved_position[0] * self.size[0] + self.game.screen_height / self.game.render_scale - 25
                     self.active_item.pos = (x,y)
                     self.inventory[self.saved_position[0]][self.saved_position[1]] = self.active_item
             else:
+                # Render legal item position and move it
                 self.active_item.render(self.game.display, offset)  
-
                 self.active_item.Move(self.game.mouse.mpos)
+                # Add item back to item list
+                if self.game.mouse.left_click == False:
+                    self.game.items.append(self.active_item)
+                    self.active_item = None
 
-
+            # Clicking on item
             if not self.game.mouse.left_click and self.item_clicked:
                         if self.item_clicked < 30 and self.game.mouse.hold_down_left < 5:
                             print("ACTIVATE")
@@ -60,8 +69,8 @@ class Inventory:
         
     
     
+    # Create a new instance of Item with the same attributes
     def clone(self):
-        # Create a new instance of Item with the same attributes
         new_item = Item(self.game, self.pos, self.type, self.quality)
         new_item.active = self.active
         new_item.animation = self.animation
@@ -69,10 +78,12 @@ class Inventory:
         new_item.size = self.size
         return new_item
 
+    # Add item to the inventory
     def Add_Item(self, item):
         for j in range(self.y_size):
             for i in range(self.x_size):
                 if not self.inventory[j][i]:
+                    # Calculate inventory position for item.pos
                     x = i * self.size[1] + self.game.screen_width / 2 / self.game.render_scale - 40
                     y = j * self.size[0] + self.game.screen_height / self.game.render_scale - 25
                     item.pos = (x, y)
@@ -94,7 +105,3 @@ class Inventory:
                 pygame.draw.rect(surf, black, pygame.Rect(x, y, self.size[0], self.size[1]), 1)
                 if self.inventory[j][i]:
                     self.inventory[j][i].render(surf, (0, 0))
-
-        # for row in self.inventory:
-        #     for item in row:
-        #         item.render(surf)         
--- scripts/Chest/item.py
+++ scripts/Chest/item.py
@@ -29,15 +29,17 @@ class Item:
             self.animation_cooldown = 50
             self.animation = random.randint(0,8)
     
-    def Move_Legal(self, mouse, player_pos, tilemap):
-        mouse_pos = mouse.mpos
+    # Check for out of bounds, return true if valid, else false
+    def Move_Legal(self, mouse_pos, player_pos, tilemap):
 
         distance = math.sqrt((player_pos[0] - mouse_pos[0]) ** 2 + (player_pos[1] - mouse_pos[1]) ** 2)
+        # Check if distance is legal, update to account for player strength later
         if distance < 40:
+            # Check if it it touches a floor tile
             for rect in tilemap.floor_rects_around(mouse_pos):
                 if not self.rect().colliderect(rect):
                     return False
-
+            # Check for walls
             for rect in tilemap.physics_rects_around(mouse_pos):
                 if self.rect().colliderect(rect):
                     return False
@@ -45,20 +47,23 @@ class Item:
         else:
             return False
     
-    def Move(self, mouse_pos):
-            self.pos = mouse_pos
+    # Update position
+    def Move(self, new_pos):
+            self.pos = new_pos
 
 
     def rect(self):
         return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
     
+    # Rener legal position
     def render(self, surf, offset=(0, 0)):
         item_image = pygame.transform.scale(self.game.assets[self.type][self.animation], self.size)
         
         surf.blit(item_image, (self.pos[0] - offset[0], self.pos[1] - offset[1]))
 
-
+    # Render item with fadeout
     def render_out_of_bounds(self, player_pos, mouse_pos, surf, offset = (0,0)):
+        # Calclate distance between player and mouse
         distance = 100 - math.sqrt((player_pos[0] - mouse_pos[0]) ** 2 + (player_pos[1] - mouse_pos[1]) ** 2)
         item_image = self.game.assets[self.type][self.animation].convert_alpha()
         item_image.set_alpha(distance)
@@ -66,5 +71,5 @@ class Item:
 
 
         
-        
+        # Render on Mouse position as the item position is not being updated
         surf.blit(item_image, (mouse_pos[0] - offset[0], mouse_pos[1] - offset[1]))
