64e4e824be9f6ef8ab73d60ca9af30c7cd259c40
RasmusLC1 <145757764+RasmusLC1@users.noreply.github.com>
2025-02-06 12:15:29 +0100

    Decoration handler refactor

--- scripts/decoration/decoration_handler.py
+++ scripts/decoration/decoration_handler.py
@@ -6,7 +6,6 @@ from scripts.decoration.boss_room.boss_room import Boss_Room
 import random
 import math
 
-
 class Decoration_Handler():
     def __init__(self, game) -> None:
         self.game = game       
@@ -15,36 +14,47 @@ class Decoration_Handler():
         self.nearby_decoration_cooldown = 0
         self.saved_data = {}
 
+        self.spawn_methods = {
+            'door': self.Spawn_Door,
+            'chest': self.Spawn_Chest,
+            'rune_shrine': self.Spawn_Rune_Shrine,
+            'bones': self.Spawn_Bones,
+            'boss_room': self.Spawn_Boss_Room
+        }
+
+        self.opening_methods = {
+            'chest': self.Open_Chest,
+            'door': self.Open_Door,
+            'rune_shrine': self.Open_Shrine,
+        }
+
     def Clear_Decorations(self):
         self.decorations.clear()
         self.saved_data.clear()
 
-
-    # TODO: Implement Level depth for boss room, chest and Shrine
-    def Initialise(self, depth = 0):
+    def Initialise(self, depth=0):
         # door initialisation
         for door in self.game.tilemap.extract([('Door_Basic', 0)].copy(), True):
             size = (self.game.assets[door.type][0].get_width(), self.game.assets[door.type][0].get_height())
-            self.Spawn_Door(door.pos, size)
+            self.Decoration_Spawner('door', door.pos, size=size)
 
         for chest in self.game.tilemap.extract([('Chest', 0)]):
             version = self.Set_Chest_Version(depth)
-            self.Spawn_Chest(chest.pos, version)
+            self.Decoration_Spawner('chest', chest.pos, version=version)
 
         for shrine in self.game.tilemap.extract([('rune_shrine', 0)]):
-            self.Spawn_Rune_Shrine(shrine.pos)
+            self.Decoration_Spawner('rune_shrine', shrine.pos)
 
         for bones in self.game.tilemap.extract([('Bones', 0)]):
-            self.Spawn_Bones(bones.pos)
+            self.Decoration_Spawner('bones', bones.pos)
 
         for boss_room in self.game.tilemap.extract([('Boss_Room', 0)]):
             temp_level = 3
             radius = random.randint(5, 7)
-            self.Spawn_Boss_Room(boss_room.pos, radius, temp_level)
+            self.Decoration_Spawner('boss_room', boss_room.pos, radius=radius, level=temp_level)
 
         for weapon in self.game.tilemap.extract([('Weapon', 0)]):
             self.game.item_handler.weapon_handler.Spawn_Random_Weapon(weapon.pos)
-
 
     def Set_Chest_Version(self, depth):
         i = 0
@@ -54,7 +64,6 @@ class Decoration_Handler():
                 break
             i += 1
         return version  
-        
 
     def Save_Decoration_Data(self):
         for decoration in self.decorations:
@@ -63,67 +72,57 @@ class Decoration_Handler():
 
     def Load_Data(self, data):
         for ID, item_data in data.items():
-            
             if not item_data:
                 continue
             try:
                 type = item_data['type']
                 pos = item_data['pos']
-                size = item_data['size']
-                decoration = None
-                if type == 'door':
-                    decoration = self.Spawn_Door(pos, size)
-                elif type == 'chest':
-                    version = item_data['version']
-                    decoration = self.Spawn_Chest(pos, version)
-                elif type == 'rune_shrine':
-                    decoration = self.Spawn_Rune_Shrine(pos)
-                elif type == 'bones':
-                    decoration = self.Spawn_Bones(pos)
-                elif type == 'boss_room':
-                    radius = item_data['radius']
-                    level = item_data['level']
-                    decoration = self.Spawn_Boss_Room(pos, radius, level)
-                else:
-                    print(type)
-                decoration.Load_Data(item_data)
-                self.decorations.append(decoration)
-
+                size = item_data.get('size')
+                version = item_data.get('version')
+                radius = item_data.get('radius')
+                level = item_data.get('level')
+                self.Decoration_Spawner(type, pos, size=size, version=version, radius=radius, level=level, data=item_data)
             except Exception as e:
                 print("DATA WRONG DECORATION HANDLER", item_data, e)
+
+    def Decoration_Spawner(self, type, pos, size=None, version=None, radius=None, level=None, data=None):
+        spawn_function = self.spawn_methods.get(type)
+        if not spawn_function:
+            print(f"Warning: Decoration type '{type}' not recognized. Decoration_Handler Decoration_Spawner")
+            return None
+
+        decoration = spawn_function(pos, size, version, radius, level)
+        if decoration:
+            if data:
+                decoration.Load_Data(data)
+            self.decorations.append(decoration)
+        return decoration
+
+    def Spawn_Door(self, pos, size, version=None, radius=None, level=None):
+        door = Door(self.game, 'door', pos, size)
+        return door
+
+    def Spawn_Chest(self, pos, size=None, version=None, radius=None, level=None):
+        chest = Chest(self.game, pos, version)  
+        return chest
+
+    def Spawn_Rune_Shrine(self, pos, size=None, version=None, radius=None, level=None):
+        shrine = Rune_Shrine(self.game, pos)  
+        return shrine
+
+    def Spawn_Bones(self, pos, size=None, version=None, radius=None, level=None):
+        bones = Bones(self.game, pos, None)  
+        self.bones.append(bones)
+        return bones
+
+    def Spawn_Boss_Room(self, pos, size=None, version=None, radius=None, level=None):
+        boss_room = Boss_Room(self.game, pos, radius, level)  
+        return boss_room
 
     def Update(self):
         self.Check_Keyboard_Input()
         for decoration in self.decorations:
             decoration.Update()
-
-
-    def Spawn_Door(self, pos, size):
-        door = Door(self.game, 'door', pos, size)
-        self.decorations.append(door)
-        return door
-
-    def Spawn_Chest(self, pos, version):
-        chest = Chest(self.game, pos, version)  
-        self.decorations.append(chest)
-        return chest
-
-    
-    def Spawn_Rune_Shrine(self, pos):
-        shrine = Rune_Shrine(self.game, pos)  
-        self.decorations.append(shrine)
-        return shrine
-    
-    def Spawn_Bones(self, pos):
-        bones = Bones(self.game, pos, None)  
-        self.decorations.append(bones)
-        self.bones.append(bones)
-        return bones
-    
-    def Spawn_Boss_Room(self, pos, radius, level):
-        boss_room = Boss_Room(self.game, pos, radius, level)  
-        self.decorations.append(boss_room)
-        return boss_room
 
     def Check_Keyboard_Input(self):
         if self.game.keyboard_handler.e_pressed:
@@ -131,30 +130,24 @@ class Decoration_Handler():
                 return
             self.game.keyboard_handler.Set_E_Key(False)
 
-
     def Check_Decorations(self):
-        nearby_decorations = self.Find_Nearby_Decorations(self.game.player.pos, 1)
+        nearby_decorations = self.Find_Nearby_Decorations(self.game.player.pos, 2)
         if not nearby_decorations:
             return False
         self.Open_Decoration(nearby_decorations)
         return True
 
     def Find_Nearby_Decorations(self, player_pos, max_distance):
-        
-
         nearby_decorations = []
         if max_distance <= 5:
             nearby_decorations = self.game.tilemap.Search_Nearby_Tiles(max_distance, player_pos, 'decoration')
-            
         else:
             nearby_decorations = self.Find_Nearby_Decorations_Long_Distance(player_pos, max_distance)
-        
         return nearby_decorations
-    
+
     def Find_Nearby_Decorations_Long_Distance(self, player_pos, max_distance):
         nearby_decorations = []
         for decoration in self.decorations:
-            # Calculate the Euclidean distance
             distance = math.sqrt((player_pos[0] - decoration.pos[0]) ** 2 + (player_pos[1] - decoration.pos[1]) ** 2)
             if distance < max_distance:
                 nearby_decorations.append(decoration)
@@ -169,77 +162,51 @@ class Decoration_Handler():
         player_pos = self.game.player.pos
         decorations.sort(key=lambda decoration: math.sqrt((player_pos[0] - decoration.pos[0]) ** 2 + (player_pos[1] - decoration.pos[1]) ** 2))
         decoration = decorations[0]
-        if self.Nearby_Chest(decoration):
-            return True
-        
-        if self.Nearby_Door(decoration):
-            return True
+        decoration_opener = self.opening_methods.get(decoration.type)
+        if not decoration_opener:
+            return
+        decoration_opener(decoration)
 
-        if self.Nearby_Shrine(decoration):
-            return True
-        
-        return False
-    
-    def Nearby_Chest(self, decoration):
-        if decoration.type == 'chest':
+    def Open_Chest(self, decoration):
+        decoration.Open()
+
+    def Open_Door(self, decoration, key=True):
+        if key:
+            if not self.Open_Door_With_Key(decoration):
+                return
+        else:
             decoration.Open()
-            if not decoration in self.decorations:
-                return False
             
-            self.decorations.remove(decoration)
-            del(decoration)
-            return True
 
-    def Nearby_Door(self, decoration, key = True):
-        if decoration.type == 'door':
-            if key:
-                if not self.Open_Door_With_Key(decoration):
-                    return False
-            else:
-                decoration.Open()
-            return True
-        
-        return False
+    def Open_Shrine(self, decoration):
+        decoration.Open()
 
-    def Nearby_Shrine(self, decoration):
-        if  'shrine' in decoration.type:
-            decoration.Open()
-            return True
-            
-        return False
-    
     def Sort_Decorations(self, decorations):
         player_pos = self.game.player.pos
         decorations.sort(key=lambda decoration: math.sqrt((player_pos[0] - decoration.pos[0]) ** 2 + (player_pos[1] - decoration.pos[1]) ** 2))
         return decorations
 
-
     def Open_Door_With_Key(self, door):
         key_found = False
         for inventory_slot in self.game.item_inventory.inventory:
-                if not inventory_slot.item:
-                    continue
-                
-                if inventory_slot.item.type == 'key':
-                    inventory_slot.Remove_Item()
-                    key_found = True
-                    break
-
+            if not inventory_slot.item:
+                continue
+            if inventory_slot.item.type == 'key':
+                inventory_slot.Remove_Item()
+                key_found = True
+                break
         if not key_found:
             return False
-            
         door.Open()
         return True
-    
+
     def Open_Door_Without_Key(self):
         decorations = self.Find_Nearby_Decorations(self.game.player.pos, 3)
         sorted_decorations = self.Sort_Decorations(decorations)
         for decoration in sorted_decorations:
             if decoration.type == 'door':
-                return self.Nearby_Door(decoration, False)
-        
+                return self.Open_Door(decoration, False)
         return False
-
 
     def Add_Decoration(self, decoration):
         if decoration in self.decorations:
@@ -251,7 +218,6 @@ class Decoration_Handler():
             self.decorations.remove(decoration)
             self.game.item_handler.Remove_Item(decoration)
             self.game.tilemap.Remove_Entity_From_Tile(decoration.tile, decoration.ID)
-
 
     def Remove_Bones(self, bones):
         self.bones.remove(bones)
